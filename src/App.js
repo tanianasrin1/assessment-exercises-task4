ed8ac5ed774832f133ae4bc9e4eb851f07bd099a
tanianasrin1 <tasrineva169@gmail.com>
2022-10-07 07:44:58 +0600

    footer page added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import InfoPage from './components/InfoPage/InfoPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/navber/Navigation';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navigation></Navigation>
       <InfoPage></InfoPage>
-            
+      <Footer></Footer>
     </div>
   );
 }
